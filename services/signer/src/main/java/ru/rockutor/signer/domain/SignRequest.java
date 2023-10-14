ef1de1999f59b5efcfef4ea7d6d02a77b48397af
skqwk <semen.esaev@yandex.ru>
2023-10-14 16:16:58 +0300

    signer: добавление сервиса подписания документов

---
+++ services/signer/src/main/java/ru/rockutor/signer/domain/SignRequest.java
@@ -0,0 +1,53 @@
+package ru.rockutor.signer.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+/**
+ * Запрос на подпись документа
+ */
+@Entity
+@Getter
+@Setter
+@Table(name = "T_SIGN_REQUEST")
+public class SignRequest {
+    /**
+     * Идентификатор запроса
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    /**
+     * Идентификатор документа
+     */
+    private UUID documentId;
+
+    /**
+     * Дата создания запроса
+     */
+    private Instant createdAt;
+
+    /**
+     * Дата подписания документа
+     */
+    private Instant signedAt;
+
+    /**
+     * Дата отзыва документа
+     */
+    private Instant canceledAt;
+
+    /**
+     * Статус запроса
+     */
+    private SignStatus status;
+}
---
+++ services/signer/src/test/java/ru/rockutor/signer/SignerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rockutor.signer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
